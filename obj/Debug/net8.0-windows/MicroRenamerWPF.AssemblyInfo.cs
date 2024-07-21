fdfc528e65695c8301e16f960683a8bd44122146
Michelle Nesbitt <mn218@nerdymail.com>
2024-07-21 07:15:40 -0400

    Fix Title

--- obj/Debug/net8.0-windows/MicroRenamerWPF.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MicroRenamerWPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroRenamerWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20880302c17fcc9edc60752cd4dc4f5843e3e3b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be062fe29333fa480dcbc7e177cfe1b61db4905d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroRenamerWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroRenamerWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
